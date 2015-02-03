8d71e7c74b262e2f511a678f1778789f908ab2cb
Zishan Ahmad <zahmad@teladoc.com>
2015-02-02 22:03:31 -0500

    output to spreadsheet

--- Gemfile
+++ Gemfile
@@ -1,4 +1,5 @@
 gem 'leankitkanban', git: 'https://github.com/zishan/leankitkanban.git'
 gem 'nokogiri'
+gem 'spreadsheet', git: 'https://github.com/zdavatz/spreadsheet.git'
 
 gem 'pry-debugger'
--- leankit-report.rb
+++ leankit-report.rb
@@ -1,5 +1,8 @@
+require 'date'
 require 'leankitkanban'
 require 'nokogiri'
+require 'spreadsheet'
+
 require 'pry-debugger'
 
 ::PRIORITIES = {
@@ -14,13 +17,6 @@ LeanKitKanban::Config.email = 'your.login@email.com'
 LeanKitKanban::Config.password = 'your.password'
 LeanKitKanban::Config.account = 'YOURCOMPANY.LEANKIT.COM'
 LeanKitKanban::Config.debug = false
-
-# SET UP DESIRED BOARD
-::LEANKIT_BOARD_ID = 123456789
-
-# SET UP CARD TYPES TO BE CAPTURED
-::CARD_TYPES = ['Epic', 'Defect']
-
 
 def get_tasks(board)
   board['Lanes'].map { |lane| lane['Cards'] }.flatten
@@ -45,47 +41,75 @@ def get_header_id(lanes, task)
   lane_id
 end
 
-def task_details(lanes, task)
-  external_id = task['ExternalCardID']
-  type = task['TypeName']
-  priority = PRIORITIES[task['Priority']]
-  lane = lanes[get_header_id(lanes, task)]['Title']
-  title = Nokogiri::HTML(task['Title']).text
-  assigned = task['AssignedUserName']
-  blocked = task['IsBlocked'] ? 'blocked' : 'not blocked'
-  url = "http://#{LeanKitKanban::Config.account.downcase}/Boards/View/#{LEANKIT_BOARD_ID}/#{task['Id']}"
-
-  [external_id, type, priority, lane, assigned, blocked, title, url]
+def task_details(board_id, lanes, task)
+  {
+    external_id: task['ExternalCardID'],
+    type: task['TypeName'],
+    priority: PRIORITIES[task['Priority']],
+    lane: lanes[get_header_id(lanes, task)]['Title'],
+    title: Nokogiri::HTML(task['Title']).text,
+    assigned: task['AssignedUserName'],
+    blocked: task['IsBlocked'] ? 'blocked' : 'not blocked',
+    url: "http://#{LeanKitKanban::Config.account.downcase}/Boards/View/#{board_id}/#{task['Id']}"
+  }
 end
 
-# puts "Retrieving board #{LEANKIT_BOARD_ID}"
-board = LeanKitKanban::Board.find(LEANKIT_BOARD_ID).flatten.first
+def board_summary(board_id, card_types)
+  puts "Retrieving board #{board_id}"
+  board = LeanKitKanban::Board.find(board_id).flatten.first
 
-output = []
-lanes = get_lanes(board)
-tasks = get_tasks(board)
+  output = []
+  lanes = get_lanes(board)
+  tasks = get_tasks(board)
 
-tasks.each do |task|
-  details = task_details(lanes, task)
+  tasks.each do |task|
+    details = task_details(board_id, lanes, task)
 
-  if CARD_TYPES.include? task['TypeName']
-    output << details
-    # puts "Output count is now #{output.count}"
+    if card_types.include? task['TypeName']
+      output << details
+      # puts "  Output count is now #{output.count}"
+    end
+
+    totalcount = task['TaskBoardTotalSize']
+    if !totalcount.nil? && totalcount > 1
+      # puts "  Task #{task['Id']} has subtasks..."
+      puts "  Retrieving task board for #{task['Id']}"
+      taskboard = LeanKitKanban::TaskBoard.find(board_id, task['Id']).flatten.first
+      boardlanes = get_lanes(taskboard)
+      subtasks = get_tasks(taskboard)
+      details = subtasks.select { |subtask| card_types.include? subtask['TypeName'] }.map { |subtask| task_details(board_id, boardlanes, subtask) }
+      output = output.concat(details)
+      # puts "  Output count is now #{output.count}"
+    end
   end
 
-  totalcount = task['TaskBoardTotalSize']
-  if !totalcount.nil? && totalcount > 1
-    # puts "Task #{task['Id']} has subtasks..."
-    # puts "Retrieving task board for #{task['Id']}"
-    taskboard = LeanKitKanban::TaskBoard.find(LEANKIT_BOARD_ID, task['Id']).flatten.first
-    boardlanes = get_lanes(taskboard)
-    subtasks = get_tasks(taskboard)
-    details = subtasks.select { |subtask| CARD_TYPES.include? subtask['TypeName'] }.map { |subtask| task_details(boardlanes, subtask) }
-    output = output.concat(details)
-    # puts "Output count is now #{output.count}"
-  end
+  output
 end
 
-output.each do |details|
-  puts "\"" + details.join("\",\"") + "\""
+board_id = 1234567890
+epics = board_summary(board_id, ['Epic'])
+defects = board_summary(board_id, ['Defect'])
+
+fname = "/tmp/leankit-report-#{DateTime.now.strftime('%Y%m%d')}.xls"
+puts "Writing output to #{fname}"
+
+# SET UP SPREADSHEET
+book = Spreadsheet::Workbook.new
+format = Spreadsheet::Format.new weight: bold
+sheet = book.create_worksheet name: 'Epic Status'
+sheet.row(0).default_format = format
+sheet.row(0).replace(%w(Title Blocked Lane))
+epics.each_with_index do |row, n|
+  sheet.row(n+1).replace([row[:title], row[:blocked], row[:lane]])
 end
+
+sheet = book.create_worksheet name: 'Defect Tracking'
+sheet.row(0).default_format = format
+sheet.row(0).replace(%w(ExternalID Type Priority Lane Assigned Blocked Title URL))
+defects.each_with_index do |row, n|
+  details = [row[:external_id], row[:type], row[:priority], row[:lane], row[:assigned], row[:blocked], row[:title], row[:url]]
+  sheet.row(n+1).replace(details)
+end
+book.write fname
+
+puts "Done."
